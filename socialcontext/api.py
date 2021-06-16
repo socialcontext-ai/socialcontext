e4fd9652c7813ffb1235026493b3794f89ea3860
scott2b <@scott2b>
2021-06-15 20:32:14 -0400

    black api.py

--- socialcontext/api.py
+++ socialcontext/api.py
@@ -5,73 +5,72 @@ import logging
 import urllib
 import oauthlib
 import os
-import requests
 import sys
 from pathlib import Path
 from cryptography.fernet import Fernet
-from functools import partial
 from oauthlib.oauth2 import BackendApplicationClient
 from oauthlib.oauth2.rfc6749.errors import MissingTokenError
 from requests_oauthlib import OAuth2Session
 
-VERSION = 'v1'
+VERSION = "v1"
 
 DEFAULT_BATCH_SIZE = 1000
 MIN_BATCH_SIZE = 500
 MAX_BATCH_SIZE = 5000
 
-logger = logging.getLogger('socialcontext')
+logger = logging.getLogger("socialcontext")
 logger.addHandler(logging.StreamHandler(sys.stdout))
 logger.setLevel(logging.DEBUG)
+
 
 class InvalidRequest(Exception):
     ...
 
+
 class Unauthorized(Exception):
     ...
+
 
 class MissingToken(Exception):
     ...
 
 
-KEY_DB = Path(__file__).parent / 'key'
+KEY_DB = Path(__file__).parent / "key"
 
 
 models = [
-    'antivax',
-    'climate_action',
-    'crime_violence',
-    'diversity',
-    'elite',
-    'emerging',
-    'fake_news',
-    'female_sports',
-    'fetch_error',
-    'gender_equality',
-    'injuries',
-    'latinx',
-    'lgbt',
-    'low_cred',
-    'male_sports',
-    'mental_health',
-    'military',
-    'online_partisan',
-    'political',
-    'profanity',
-    'provax',
-    'renewable_energy',
-    'sexually_explicit',
-    'traditional',
-    'vice',
-    'wire'
+    "antivax",
+    "climate_action",
+    "crime_violence",
+    "diversity",
+    "elite",
+    "emerging",
+    "fake_news",
+    "female_sports",
+    "fetch_error",
+    "gender_equality",
+    "injuries",
+    "latinx",
+    "lgbt",
+    "low_cred",
+    "male_sports",
+    "mental_health",
+    "military",
+    "online_partisan",
+    "political",
+    "profanity",
+    "provax",
+    "renewable_energy",
+    "sexually_explicit",
+    "traditional",
+    "vice",
+    "wire",
 ]
 
 
-class SocialcontextClient():
+class SocialcontextClient:
 
-    API_ROOT = os.environ.get(
-        'SOCIALCONTEXT_API_ROOT',
-        'https://api.socialcontext.ai')
+    API_ROOT = os.environ.get("SOCIALCONTEXT_API_ROOT", "https://api.socialcontext.ai")
     TOKEN_URL = f"{API_ROOT}/{VERSION}/token"
     REFRESH_URL = f"{API_ROOT}/{VERSION}/token-refresh"
     _news = None
@@ -97,10 +96,11 @@ class SocialcontextClient():
                 token_url=self.TOKEN_URL,
                 client_id=self.app_id,
                 client_secret=self.app_secret,
-                include_client_id=True)
+                include_client_id=True,
+            )
         except MissingTokenError:
             # oauthlib gives the same error regardless of the problem
-            print('Something went wrong, please check your client credentials.')
+            print("Something went wrong, please check your client credentials.")
             raise
         return token
 
@@ -110,7 +110,8 @@ class SocialcontextClient():
             token=token,
             auto_refresh_url=self.REFRESH_URL,
             auto_refresh_kwargs={},
-            token_updater=self.token_saver)
+            token_updater=self.token_saver,
+        )
 
     def fernet(self, key):
         _key = key + "=" * (len(key) % 4)
@@ -124,14 +125,14 @@ class SocialcontextClient():
         return self.fernet(self.app_secret).decrypt(data)
 
     def token_saver(self, token):
-        logger.debug('SAVING TOKEN: %s' % str(token))
+        logger.debug("SAVING TOKEN: %s" % str(token))
         data = json.dumps(token)
         _t = self.encrypt(data)
-        with dbm.open(KEY_DB.as_posix(), 'c') as db:
+        with dbm.open(KEY_DB.as_posix(), "c") as db:
             db[self.app_id] = _t
 
     def load_saved_token(self):
-        with dbm.open(KEY_DB.as_posix(), 'c') as db:
+        with dbm.open(KEY_DB.as_posix(), "c") as db:
             try:
                 token = db[self.app_id]
             except KeyError:
@@ -140,24 +141,24 @@ class SocialcontextClient():
         return token
 
     def clear_saved_token(self):
-        with dbm.open(KEY_DB.as_posix(), 'c') as db:
+        with dbm.open(KEY_DB.as_posix(), "c") as db:
             if self.app_id in db:
-                del(db[self.app_id]) 
+                del db[self.app_id]
 
     def dispatch(self, method, url, data=None, **query):
         logger.debug(f"Fetching URL {url}; method: {method}")
         query = urllib.parse.urlencode(query)
         try:
-            if method =='get':
-                resp = self.client.get(f'{url}?{query}')
-            elif method == 'post':
+            if method == "get":
+                resp = self.client.get(f"{url}?{query}")
+            elif method == "post":
                 resp = self.client.post(url, json=data)
-            elif method == 'put':
+            elif method == "put":
                 resp = self.client.put(url, json=data)
-            elif method == 'delete':
+            elif method == "delete":
                 resp = self.client.delete(url, json=data)
             else:
-                raise Exception('Unsupported dispatch method')
+                raise Exception("Unsupported dispatch method")
             if resp.status_code in [400, 401, 403]:
                 raise MissingToken
             return resp
@@ -166,130 +167,98 @@ class SocialcontextClient():
             token = self.fetch_api_token()
             self.token_saver(token)
             self.client = self.create_client_for_token(token)
-            if method == 'get':
-                return self.client.get(f'{url}?{query}')
-            elif method == 'post':
+            if method == "get":
+                return self.client.get(f"{url}?{query}")
+            elif method == "post":
                 return self.client.post(url, json=data)
-            elif method == 'put':
+            elif method == "put":
                 return self.client.put(url, json=data)
-            elif method == 'delete':
+            elif method == "delete":
                 return self.client.delete(url, json=data)
             else:
-                raise Exception('Unsupported dispatch method')
+                raise Exception("Unsupported dispatch method")
 
     def get(self, url, **query):
-        r = self.dispatch('get', url, **query)
+        r = self.dispatch("get", url, **query)
         return r
 
     def post(self, url, data=None):
-        return self.dispatch('post', url, data=data)
+        return self.dispatch("post", url, data=data)
 
     def put(self, url, data=None):
-        return self.dispatch('put', url, data=data)
+        return self.dispatch("put", url, data=data)
 
     def delete(self, url):
-        return self.dispatch('delete', url)
+        return self.dispatch("delete", url)
 
     def pathget(self, path, version=VERSION, **query):
         prefix = self.prefix(version)
-        url = f'{prefix}/{path}'
-        return self.get(url, **query) 
+        url = f"{prefix}/{path}"
+        return self.get(url, **query)
 
     def pathpost(self, path, version=VERSION, data=None):
         prefix = self.prefix(version)
-        url = f'{prefix}/{path}'
+        url = f"{prefix}/{path}"
         return self.post(url, data=data)
 
     def pathput(self, path, version=VERSION, data=None):
         prefix = self.prefix(version)
-        url = f'{prefix}/{path}'
+        url = f"{prefix}/{path}"
         return self.put(url, data=data)
 
     def pathdelete(self, path, version=VERSION):
         prefix = self.prefix(version)
-        url = f'{prefix}/{path}'
+        url = f"{prefix}/{path}"
         return self.delete(url)
 
     # API endpoints
 
-    #def classify(self, content_type, *, models=None, url=None, text=None, version='v0.1a'):
-    #    if url:
-    #        reqtype = 'url'
-    #        content = url
-    #    elif text:
-    #        reqtype = 'text'
-    #        content = text
-    #    else:
-    #        raise InvalidRequest('url or text parameter required')
-    #    r = self.pathpost(f'{content_type}/classify', version, data={
-    #        reqtype: content, 'models': models
-    #    })
-    #    if r.status_code == 403:
-    #        raise Unauthorized
-    #    return r
-
-    #def account_info(self, version='v0.1a'):
-    #    """Get info for user account."""
-    #    r = self.pathget('account', version)
-    #    return r
-
-
-    def create_job(self, *, content_type='news', input_file=None,
-              output_path=None, models=None, batch_size=DEFAULT_BATCH_SIZE,
-              options=None, version=VERSION):
+    def create_job(
+        self,
+        *,
+        content_type="news",
+        input_file=None,
+        output_path=None,
+        models=None,
+        batch_size=DEFAULT_BATCH_SIZE,
+        options=None,
+        version=VERSION,
+    ):
         """Create a batch processing job."""
         if models is None:
             models = []
         if options is None:
             options = []
         if input_file is None:
-            raise Exception('Input file required.')
-        r = self.pathpost('jobs', version, data={
-            'content_type': content_type,
-            'input_file': input_file,
-            'output_path': output_path,
-            'batch_size': batch_size,
-            'options': options,
-            'models': models
-        })
+            raise Exception("Input file required.")
+        r = self.pathpost(
+            "jobs",
+            version,
+            data={
+                "content_type": content_type,
+                "input_file": input_file,
+                "output_path": output_path,
+                "batch_size": batch_size,
+                "options": options,
+                "models": models,
+            },
+        )
         return r
 
     def update_job(self, job_name, *, version=VERSION, **data):
         """Update a pre-defined job."""
-        r = self.pathput(f'jobs/{job_name}', version, data=data)
+        r = self.pathput(f"jobs/{job_name}", version, data=data)
         return r
 
     def delete_job(self, job_name, *, version=VERSION):
         """Delete a job."""
-        r = self.pathdelete(f'jobs/{job_name}', version)
+        r = self.pathdelete(f"jobs/{job_name}", version)
         return r
 
     def jobs(self, *, job_name=None, version=VERSION):
         """List jobs or show details of a specified job."""
         if job_name:
-            r = self.pathget(f'jobs/{job_name}', version)
+            r = self.pathget(f"jobs/{job_name}", version)
         else:
-            r = self.pathget(f'jobs', version)
+            r = self.pathget(f"jobs", version)
         return r
-
-    #def makeclient(self, client_name, version='v0.1'):
-    #    r = self.pathpost('clients', version, data={
-    #        'name': client_name}) 
-    #    if r.status_code == 403:
-    #        raise Unauthorized
-    #    return r
-
-    #def clients(self, version='v0.1'):
-    #    r = self.pathget('clients', version)
-    #    if r.status_code == 403:
-    #        raise Unauthorized
-    #    return r
-
-    #@property
-    #def news(self):
-    #    if self._news is None:
-    #        self._news = type('news', (object,), {
-    #            'classify': partial(self.classify, 'news'),
-    #            'batch': partial(self.batch, 'news')
-    #        })
-    #    return self._news()
